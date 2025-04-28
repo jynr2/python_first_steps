d63244194d95d1d44b22274bf4d4e58b5e5cc665
jyn <jhonjyn1@gmail.com>
2025-04-28 10:27:42 -0500

    formateo de cadenas

--- basic/04_strings.py
+++ basic/04_strings.py
@@ -1,3 +1,20 @@
 
 my_string = 'Mi string'
-my_other_string = 'Mi string'
+my_other_string = 'Mi otro string'
+
+print(my_string)
+print(len(my_other_string))
+print(my_string + ' ' + my_other_string)
+
+my_new_line_string = 'este es un string \ncon salto de linea'
+print(my_new_line_string)
+
+my_tab_string = '\teste es un string con tabulación'
+print(my_tab_string)
+
+my_scape_tab_string = '\teste es un string \n con escape'
+print(my_scape_tab_string)
+
+# formateo
+name, surname, age = 'jyn', 'test', 32
+print('Mi nombre es {}')
