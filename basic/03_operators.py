5dd48bacaeebb1f915652ee1b83c4bb2f48f516b
jyn <jhonjyn1@gmail.com>
2025-04-28 09:36:48 -0500

    manejo strings 2:25

---
+++ basic/03_operators.py
@@ -0,0 +1,45 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(10/3)
+print(10%2) # resto de una división
+print(10//3)
+print(2 ** 3) # elevar a la x
+
+# usar + para concantar cadenas de texto
+print('hola' + ' python')
+print('hola ' + str(5))
+
+#multiplicar cadenas
+print('hola ' * 5)
+
+# operadores de comparación
+print(3 > 4)
+print(3 >= 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+
+print('hola' > 'python')
+print('hola' >= 'python')
+print('hola' < 'python')
+print('hola' <= 'python')
+print('hola' == 'python')
+print('hola' != 'python')
+print('HOLA' == 'hola')
+print('abc' == 'def')  # comparación alfabetica
+
+# operadores lógicos
+print(3 > 4 and 'hola' > 'python')
+print(3 > 4 or 'hola' > 'python')
+print(3 < 4 and 'hola' < 'python')
+print(3 < 4 or 'hola' > 'python')
+print(not 3 > 4)
+
+x = 10  # 10 en binario es 1010
+x >>= 2  # Desplaza 1010 dos posiciones a la derecha, resultado: 0010 (que es 2 en decimal)
+print(x)  # Output: 2
+
+
---
+++ basic/04_strings.py
@@ -0,0 +1,3 @@
+
+my_string = 'Mi string'
+my_other_string = 'Mi string'
