c4110eb49979f0ee60b8a51422db87f364e452d5
jyn <jhonjyn1@gmail.com>
2025-05-15 20:41:35 -0500

    end modules

---
+++ basic/14_modules.py
@@ -0,0 +1,9 @@
+### modules ###
+
+#import module as m
+from math import pi
+from module import sum_three_values, print_name
+
+print(pi)
+sum_three_values(3,4,10)
+print_name('John', 'Doe')
---
+++ basic/module.py
@@ -0,0 +1,5 @@
+def sum_three_values(num1:int, num2:int, num3:int):
+    print(num1 + num2 + num3)
+
+def print_name(name: str, surname:str):
+    print(f'{name} {surname}')
